b5531b488986519274acbbe83b4b2272e0e57ecb
yozica <lichengmingisme@foxmail.com>
2023-08-19 16:05:32 +0800

    集成swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger文档配置
+  const config = new DocumentBuilder()
+    .setTitle('Yozica example')
+    .setDescription('The yozica API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

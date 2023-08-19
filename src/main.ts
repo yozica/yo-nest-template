cd89c3ef4e3364ff69b401b08daa2889134aff6b
yozica <lichengmingisme@foxmail.com>
2023-08-19 18:24:51 +0800

    添加全局异常过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 全局异常过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   // swagger文档配置
   const config = new DocumentBuilder()
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { ResType } from 'src/type';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Get('getUserById/:id')
+  getUserById(@Param() { id }): ResType {
+    id = parseInt(id);
+
+    if (id === 0) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          message: '请求参数id不能为0',
+          error: 'id can not be zero',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return {
+      code: 0,
+      msg: '请求成功',
+      data: null,
+    };
+  }
 }

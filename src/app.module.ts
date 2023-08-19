ab84ef086cc7305d98d05bbf616ab6b661c7a6dd
yozica <lichengmingisme@foxmail.com>
2023-08-19 17:56:38 +0800

    自定义logger中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { HelloModule, UserModule } from './modules';
+import { LoggerMiddleWare } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [HelloModule, UserModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleWare)
+      // .exclude({ path: 'hello', method: RequestMethod.GET })
+      .forRoutes('hello');
+  }
+}
